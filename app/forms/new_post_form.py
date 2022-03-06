72fc1c96f20824897e0c1c905be7631d45619027
tombetthauser <tombetthauser@gmail.com>
2022-03-06 01:58:05 -0800

    Add image url validation

--- app/forms/new_post_form.py
+++ app/forms/new_post_form.py
@@ -1,7 +1,8 @@
+from wsgiref.validate import validator
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, ValidationError
-from app.models import Page
+from app.models import Page, Post
 
 def bad_page_id(form, field):
     pageId = int(field.data)
@@ -9,9 +10,16 @@ def bad_page_id(form, field):
     if not page:
         raise ValidationError('Page Id does not exist.')
 
+def image_url_used(form, field):
+  image_url = field.data
+  post = Post.query.filter(Post.imageUrl == image_url).first()
+  if post:
+      raise ValidationError('Image Url already in use.')
+
+
 class NewPostForm(FlaskForm):
   pageId = StringField('pageId', validators=[DataRequired(), bad_page_id])
-  imageUrl = StringField('imageUrl')
+  imageUrl = StringField('imageUrl', validators=[image_url_used])
   title = StringField('title')
   text = StringField('text')
   location = StringField('location')
