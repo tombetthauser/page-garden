1c73b84554ff0902d2ffb8595e2558c23cfd37a9
tombetthauser <tombetthauser@gmail.com>
2022-03-30 17:05:08 -0700

    Add fields to component

--- react-app/src/components/PostCreateAWS.js
+++ react-app/src/components/PostCreateAWS.js
@@ -11,6 +11,16 @@ const PostCreateAWS = () => {
   const [imageLoading, setImageLoading] = useState(false);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+  const [id, setId] = useState('');
+  const [pageId, setPageId] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [aspectRatio, setAspectRatio] = useState('');
+  const [imageRotation, setImageRotation] = useState('');
+  const [text, setText] = useState('');
+  const [location, setLocation] = useState('');
+  const [linkText, setLinkText] = useState('');
+  const [linkUrl, setLinkUrl] = useState('');
+  const [date, setDate] = useState('');
 
   const history = useHistory();
 
@@ -25,6 +35,7 @@ const PostCreateAWS = () => {
 
   const onSubmit = async (e) => {
     e.preventDefault();
+    alert("Hello!");
   };
 
   const updateImage = (e) => {
@@ -37,16 +48,25 @@ const PostCreateAWS = () => {
     <post-create-aws>
       <form onSubmit={onSubmit}>
         { imagePreview ? <img src={imagePreview} alt="image preview"/> : null }
-        <input
-          type="file"
-          accept="image/*"
-          onChange={updateImage}
-        />
+
+        <input type="file" accept="image/*" onChange={updateImage} />
+        
         <div><label>Title</label><input type='text' name='title' onChange={setTitle} value={title}></input></div>
         <div><label>Description</label><input type='text' name='title' onChange={setDescription} value={description}></input></div>
+        <div><label>Id</label><input type='text' name='title' onChange={setId} value={id}></input></div>
+        <div><label>PageId</label><input type='text' name='title' onChange={setPageId} value={pageId}></input></div>
+        <div><label>ImageUrl</label><input type='text' name='title' onChange={setImageUrl} value={imageUrl}></input></div>
+        <div><label>AspectRatio</label><input type='text' name='title' onChange={setAspectRatio} value={aspectRatio}></input></div>
+        <div><label>ImageRotation</label><input type='text' name='title' onChange={setImageRotation} value={imageRotation}></input></div>
+        <div><label>Text</label><input type='text' name='title' onChange={setText} value={text}></input></div>
+        <div><label>Location</label><input type='text' name='title' onChange={setLocation} value={location}></input></div>
+        <div><label>LinkText</label><input type='text' name='title' onChange={setLinkText} value={linkText}></input></div>
+        <div><label>LinkUrl</label><input type='text' name='title' onChange={setLinkUrl} value={linkUrl}></input></div>
+        <div><label>Date</label><input type='text' name='title' onChange={setDate} value={date}></input></div>
+
 
         <button type="submit">Submit</button>
-        {imageLoading ? <p>Loading...</p> : null}
+        { imageLoading ? <p>Loading...</p> : null }
       </form>
     </post-create-aws>
   );
