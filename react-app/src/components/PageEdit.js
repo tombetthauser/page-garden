b7a279fc637aab46da4189886a3554453bff8f75
tombetthauser <tombetthauser@gmail.com>
2022-04-02 11:21:05 -0700

    Style page edit

--- react-app/src/components/PageEdit.js
+++ react-app/src/components/PageEdit.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux'
-import { useHistory, useParams } from 'react-router-dom';
+import { useHistory, useParams, NavLink} from 'react-router-dom';
 
 const PageEdit = () => {
   const [page, setPage] = useState({});
@@ -116,28 +116,30 @@ const PageEdit = () => {
   // ...
 
   return (
-    <pageedit>
+    <div class="styled-form">
+      <h1>Edit Page</h1>
       <form onSubmit={onSubmit}>
-        <div>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
-        </div>
+        <div>{errors.map((error, ind) => (<div key={ind}>{error}</div>))}</div>
         <div><label>Url</label><input type='text' name='url' onChange={updateUrl} value={url}></input></div>
         <div><label>Title</label><input type='text' name='title' onChange={updateTitle} value={title}></input></div>
-        <div><label>Text</label><input type='text' name='text' onChange={updateText} value={text}></input></div>
-        <div><label>Location</label><input type='text' name='location' onChange={updateLocation} value={location}></input></div>
-        <div><label>Link 1 Url</label><input type='text' name='link1Url' onChange={updateLink1Url} value={link1Url}></input></div>
-        <div><label>Link 1 Text</label><input type='text' name='link1Text' onChange={updateLink1Text} value={link1Text}></input></div>
-        <div><label>Link 2 Url</label><input type='text' name='link2Url' onChange={updateLink2Url} value={link2Url}></input></div>
-        <div><label>Link 2 Text</label><input type='text' name='link2Text' onChange={updateLink2Text} value={link2Text}></input></div>
-        <div><label>Link 3 Url</label><input type='text' name='link3Url' onChange={updateLink3Url} value={link3Url}></input></div>
-        <div><label>Link 3 Text</label><input type='text' name='link3Text' onChange={updateLink3Text} value={link3Text}></input></div>
-        <div><label>Contact</label><input type='text' name='contact' onChange={updateContact} value={contact}></input></div>
+        <div><label>Text</label><textarea class="textbox-input" type='text' name='text' onChange={updateText} value={text}></textarea></div>
+        <details>
+          <summary>more...</summary>
+          <div><label>Location</label><input type='text' name='location' onChange={updateLocation} value={location}></input></div>
+          <div><label>Link 1 Url</label><input type='text' name='link1Url' onChange={updateLink1Url} value={link1Url}></input></div>
+          <div><label>Link 1 Text</label><input type='text' name='link1Text' onChange={updateLink1Text} value={link1Text}></input></div>
+          <div><label>Link 2 Url</label><input type='text' name='link2Url' onChange={updateLink2Url} value={link2Url}></input></div>
+          <div><label>Link 2 Text</label><input type='text' name='link2Text' onChange={updateLink2Text} value={link2Text}></input></div>
+          <div><label>Link 3 Url</label><input type='text' name='link3Url' onChange={updateLink3Url} value={link3Url}></input></div>
+          <div><label>Link 3 Text</label><input type='text' name='link3Text' onChange={updateLink3Text} value={link3Text}></input></div>
+          <div><label>Contact</label><input type='text' name='contact' onChange={updateContact} value={contact}></input></div>
+        </details>
 
         <button type='submit'>Update Page!</button>
       </form>
-    </pageedit>
+      <NavLink to={`/${page.url}`}>cancel edit</NavLink>
+      {/* <vr /><NavLink to="/home">home</NavLink> */}
+    </div>
   );
 };
 
